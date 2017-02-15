2034f7ed2789c7e5638672c4ee8c6601466f0855
clairellesage <clairellesage@gmail.com>
2017-02-15 23:37:45 +0000

    dead end for admin categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,25 @@
 class Admin::DashboardController < ApplicationController
-  def show
+  
+  def index
+  	@category = Category.all
   end
+  
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
